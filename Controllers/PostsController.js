cbe59ad7ce7edd36691b025841feeb077264e5e6
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-30 17:24:54 +0100

    edit blog

--- Controllers/PostsController.js
+++ Controllers/PostsController.js
@@ -1,36 +1,4 @@
-const posts = [
-  {
-    titolo: "Blog Express 1",
-    contenuto: "Esercizio Node/Express 1",
-    immagine: "",
-    tags: ["#tag1", "#tag2", "#tag3"],
-  },
-  {
-    titolo: "Blog Express 2",
-    contenuto: "Esercizio Node/Express 2",
-    immagine: "",
-    tags: ["#tag4", "#tag5", "#tag6"],
-  },
-  {
-    titolo: "Blog Express 3",
-    contenuto: "Esercizio Node/Express 3",
-    immagine: "",
-    tags: ["#tag7", "#tag8", "#tag9"],
-  },
-  {
-    titolo: "Blog Express 4",
-    contenuto: "Esercizio Node/Express 4",
-    immagine: "",
-    tags: ["#tag10", "#tag11", "#tag12"],
-  },
-  {
-    titolo: "Blog Express 5",
-    contenuto: "Esercizio Node/Express 5",
-    immagine: "",
-    tags: ["#tag13", "#tag14", "#tag15"],
-  },
-];
-
+const posts = require("../Data/posts.js")
 
 function index(req, res) {
     res.json({
