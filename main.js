a8e0beab558d6920a144d95032bf89eed17f0cc6
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-30 16:29:06 +0100

    edit mainjs

---
+++ main.js
@@ -0,0 +1,28 @@
+// Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità.
+
+// Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
+
+// Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+
+// Creiamo poi una rotta /posts che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array locale.
+
+// La rotta relativa ai post dovrà chiamare la funzione index() dal controller dedicato ( controllers/posts.js )
+
+// Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+
+// Testare nel browser.
+
+const express = require("express");
+const app = express();
+const port = 3000;
+const host = "http://127.0.0.1";
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on ${host}:${port}`);
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Ciao Mondo!");
+});
